79771eb13d202d96796a82ddb35f8ae154ee7fe7
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-12-16 18:49:17 -0500

    fix: add opponent

--- src/mw.py
+++ src/mw.py
@@ -1,42 +1,45 @@
 import numpy as np
 
 
-class Expert:
-    ...
-
-
 def play(p1, p2, iterations=10000, UNITS=100):
     p1 = p1 / np.sum(p1)
     p2 = p2 / np.sum(p2)
-    np.random.shuffle(p1) # only need to shuffle one
+    np.random.shuffle(p1)  # only need to shuffle one
     n = len(p1)
     assert n == len(p2)
 
-    p1_choices = np.random.choice(n, (iterations, UNITS), replace=True) # (iterations, UNITS)
-    p2_choices = np.random.choice(n, (iterations, UNITS), replace=True) # (iterations, UNITS)
-    p1_alloc = np.apply_along_axis(lambda x : np.histogram(x, bins=np.arange(n+1))[0], axis=1, arr=p1_choices) # (iterations, n)
-    p2_alloc = np.apply_along_axis(lambda x : np.histogram(x, bins=np.arange(n+1))[0], axis=1, arr=p2_choices) # (iterations, n)
+    p1_choices = np.random.choice(
+        n, (iterations, UNITS), replace=True
+    )  # (iterations, UNITS)
+    p2_choices = np.random.choice(
+        n, (iterations, UNITS), replace=True
+    )  # (iterations, UNITS)
+    p1_alloc = np.apply_along_axis(
+        lambda x: np.histogram(x, bins=np.arange(n + 1))[0], axis=1, arr=p1_choices
+    )  # (iterations, n)
+    p2_alloc = np.apply_along_axis(
+        lambda x: np.histogram(x, bins=np.arange(n + 1))[0], axis=1, arr=p2_choices
+    )  # (iterations, n)
 
-    p1_resources_won, p2_resources_won = np.sum(p1_alloc > p2_alloc, axis=1), np.sum(p2_alloc > p1_alloc, axis=1) # (iterations,)
+    p1_resources_won, p2_resources_won = np.sum(p1_alloc > p2_alloc, axis=1), np.sum(
+        p2_alloc > p1_alloc, axis=1
+    )  # (iterations,)
 
-    p1_wins = np.sum(p1_resources_won > p2_resources_won) # scalar
-    p2_wins = np.sum(p2_resources_won > p1_resources_won) # scalar
-    ties = np.sum(p1_resources_won == p2_resources_won) # scalar
+    p1_wins = np.sum(p1_resources_won > p2_resources_won)  # scalar
+    p2_wins = np.sum(p2_resources_won > p1_resources_won)  # scalar
+    ties = np.sum(p1_resources_won == p2_resources_won)  # scalar
     return p1_wins, p2_wins, ties
 
-def play_game(experts: list[Expert], player: Expert):
-    ...
 
-
-def get_loss(player: Expert, experts: list[Expert]):
-    losses = play_game(experts, player)
-    return sum(losses)
+def get_loss(player, opponent):
+    player_wins, opponent_wins = play(player, opponent)
+    return opponent_wins / (player_wins + opponent_wins)
 
 
 v_get_loss = np.vectorize(get_loss)
 
 
-def mw(experts: list[Expert], oponent: Expert, eta=0.1, T=1000):
+def mw(experts: list, opponent, eta=0.1, T=1000):
     n = len(experts)
     weights = np.ones(n)
     total_loss = 0
@@ -45,7 +48,7 @@ def mw(experts: list[Expert], oponent: Expert, eta=0.1, T=1000):
         random_expert_index = np.random.choice(n, p=weights / weights.sum())
 
         # play game for each expert against all experts (including itself)
-        losses = v_get_loss(experts, experts)
+        losses = v_get_loss(experts, opponent)
 
         # update total loss (based on expert chosen)
         total_loss += losses[random_expert_index]
