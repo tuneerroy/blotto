80df9dfa166b265d3417503a20299d6ed24013ea
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-12-16 19:08:47 -0500

    feat: way to create more experts by calling mw

--- src/experts.py
+++ src/experts.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from mw import mw
+
 
 def get_new_expert(experts, weights):
     normalized_weights = weights / np.sum(weights)
@@ -18,3 +20,12 @@ def dump_experts(experts):
     with open("experts.txt", "w") as f:
         for expert in experts:
             f.write(" ".join([str(x) for x in expert]) + "\n")
+
+
+def create_more_experts(num_new: int):
+    experts = read_in_experts()
+    for _ in range(num_new):
+        weights, _ = mw(experts, experts)
+        new_expert = get_new_expert(experts, weights)
+        experts.append(new_expert)
+    dump_experts(experts)
