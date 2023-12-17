d6b229bdc11e1f9f653c9195c4846eaba7763172
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-12-17 00:12:24 -0500

    fix: finish expert generation script

--- src/experts.py
+++ src/experts.py
@@ -1,3 +1,6 @@
+from mw import *
+from constants import *
+import os
 import numpy as np
 
 from mw import mw
@@ -9,6 +12,9 @@ def get_new_expert(experts, weights):
 
 
 def read_in_experts():
+    if not os.path.exists("experts.txt"):
+        return []
+
     experts = []
     with open("experts.txt") as f:
         for line in f:
@@ -22,10 +28,26 @@ def dump_experts(experts):
             f.write(" ".join([str(x) for x in expert]) + "\n")
 
 
-def create_more_experts(num_new: int):
-    experts = read_in_experts()
-    for _ in range(num_new):
-        weights, _ = mw(experts, experts)
+def create_more_experts(num_new: int, starting_experts=10):
+    
+    experts = np.array(read_in_experts())
+    if len(experts) < starting_experts:
+        experts = np.random.rand(starting_experts, RESOURCES)
+        starting_experts -= len(experts)
+
+    min_loss = float("inf")
+    for i in range(num_new):
+        print("Creating new expert: ", i + 1)
+        opponent = np.random.rand(RESOURCES)
+        weights, loss, _ = mw(experts, opponent, eta=ETA, T=T)
+        min_loss = min(min_loss, loss)
+        print("Loss: ", loss, " Min loss: ", min_loss)
         new_expert = get_new_expert(experts, weights)
-        experts.append(new_expert)
+        experts = np.vstack((experts, new_expert))
+        print("New expert created...")
+        print("-" * 50)
+
     dump_experts(experts)
+
+if __name__ == "__main__":
+    create_more_experts(50)
